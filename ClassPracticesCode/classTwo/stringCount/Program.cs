5495b57013fc40d40787946b7a5ca193100a14fc
MithunCh <mithun.howlader222@gmail.com>
2021-04-14 23:14:37 +0600

    linq example

---
+++ ClassPracticesCode/classTwo/stringCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace stringCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            var input = Console.ReadLine();
+
+            var Query1 =
+                from inp in input
+                orderby inp
+                group inp by inp.ToString().ToUpper();
+                               
+            foreach(var item in Query1)
+            {
+                Console.WriteLine($"{item.Key}, {item.Count()}");
+            }
+
+
+
+            //Console.WriteLine(input);
+        }
+    }
+}
---
+++ ClassPracticesCode/classTwo/weekNamePrint/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace weekNamePrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekName = new string[7];
+            weekName[0] = "Saturday";
+            weekName[1] = "Sunday";
+            weekName[2] = "Monday";
+            weekName[3] = "Tuesday";
+            weekName[4] = "Wednesday";
+            weekName[5] = "Thusday";
+            weekName[6] = "Friday";
+
+            var Query1 =
+                from week in weekName
+                where week.StartsWith("S")
+                select week;
+
+            //foreach(var item in Query1)
+            //{
+            //    Console.WriteLine($"Name: {item}");
+            //}
+
+            var Query2 =
+                from week in weekName
+                select week;
+            foreach(var item in Query2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
