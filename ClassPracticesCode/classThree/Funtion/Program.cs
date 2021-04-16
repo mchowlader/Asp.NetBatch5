031816b9eb57f6739d7f501ae8a3aaa08cd7515f
MithunCh <mithun.howlader222@gmail.com>
2021-04-17 02:06:08 +0600

    Funtion practices

---
+++ ClassPracticesCode/classThree/Funtion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Funtion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "Vegas, House - 007";
+            Address(x, Home);
+        }
+
+        public static string Home(string add)
+        {
+            Console.WriteLine($"My Address: {add}");
+            return add;
+        }
+
+        public static string Address(string address ,Func<string,string> home)
+        {
+            home(address);
+            return default;
+        }
+    }
+}
