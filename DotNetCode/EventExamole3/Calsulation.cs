097539d09b6b6da2e8041bc3cd1b027a775120e4
MithunCh <mithun.howlader222@gmail.com>
2021-04-18 17:12:33 +0600

    eventEx3 in class three

---
+++ DotNetCode/EventExamole3/Calsulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExamole3
+{
+   public class Calsulation
+    {
+        public delegate void dgCalculation();
+
+        public event dgCalculation evMath;
+
+        public void Sometthing()
+        {
+            var numOne = Convert.ToInt32(Console.ReadLine());
+            var numTwo = Convert.ToInt32(Console.ReadLine());
+            var sum = numOne + numTwo;
+
+            if(sum % 2 == 0)
+            {
+                if(evMath != null)
+                    evMath();
+
+                else
+                    Console.WriteLine($"This is a only Even Number");
+            }
+
+            else
+                Console.WriteLine($"Event not fire");
+
+        }
+    }
+}
---
+++ DotNetCode/EventExamole3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventExamole3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subscribe subscribe = new Subscribe();
+            Calsulation calsulation = new Calsulation();
+
+            calsulation.evMath += new Calsulation.dgCalculation(Subscribe.SubscribeOne);
+            calsulation.Sometthing();
+
+            calsulation.evMath -= new Calsulation.dgCalculation(Subscribe.SubscribeOne);
+            calsulation.Sometthing();
+        }
+    }
+}
