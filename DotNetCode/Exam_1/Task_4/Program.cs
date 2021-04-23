60fb35a51bdf1dd39ccb3c0f977a007cfc9adf59
MithunCh <mithun.howlader222@gmail.com>
2021-04-23 20:13:54 +0600

    Exam-1 task4

---
+++ DotNetCode/Exam_1/Task_4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_4
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Student student = new Student();
+
+            List<Student> list1 = new List<Student>()
+            {  
+                new Student{Name = "Anna", Age = 25},
+                new Student{Name = "Joseph", Age = 20},
+                new Student{Name = "Maria", Age = 30},
+                new Student{Name = "Susanna", Age = 20},
+                new Student{Name = "Andreas", Age = 49}
+            };
+            List<Student> list2 = new List<Student>()
+            { 
+                new Student{Name = "Josephi", Age = 50},
+                new Student{Name = "Sophia", Age = 40},
+                new Student{Name = "Joannes", Age = 55},
+                new Student{Name = "Jacob", Age = 30},
+                new Student{Name = "Martinus", Age = 20},
+
+            };
+
+            List<string> result = null;
+
+            var listCombined = 
+                from st in list1.Concat(list2)
+                orderby st.Name , st.Age ascending
+                select st.Name;
+
+            foreach(var item in listCombined)
+            {
+                Console.WriteLine(item);
+            }
+
+                
+            
+        }
+    }
+}
