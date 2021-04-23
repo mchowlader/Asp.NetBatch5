47856ad90232b1f2a9ac8b7559d8891d2e24a6a1
MithunCh <mithun.howlader222@gmail.com>
2021-04-23 20:31:23 +0600

    Exam_1 Task2

---
+++ DotNetCode/Exam_1/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass sampleClass = new SampleClass();
+            var type = sampleClass.GetType().Assembly;
+            var type1 = sampleClass.GetType().BaseType;
+            Console.WriteLine(type1);
+        }
+    }
+}
