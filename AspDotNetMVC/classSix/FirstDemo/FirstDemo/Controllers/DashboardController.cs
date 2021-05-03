dc003307e142fd56fafc33fe23e4f1099358e395
MithunCh <mithun.howlader222@gmail.com>
2021-05-04 01:37:40 +0600

    MVC SecondDemo

---
+++ AspDotNetMVC/classSix/FirstDemo/FirstDemo/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using FirstDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Summary()
+        {
+            var model = new SummaryModel();
+            return View(model);
+        }
+
+        public IActionResult Admin()
+        {
+
+            return View();
+        }
+    }
+}
