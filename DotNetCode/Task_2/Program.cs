8a2d143c4b6d229dca9e75fa759d70c0fc81d6de
MithunCh <mithun.howlader222@gmail.com>
2021-05-06 01:12:08 +0600

    save

---
+++ DotNetCode/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    public class SmartSort
+    {
+        public delegate int Compare<T>(T a, T b);
+
+        public static IList<T> Sort<T>(IList<T> elements, Compare<T> compare )
+        {
+            return default;
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+        }
+
+
+    }
+}
---
+++ DotNetCode/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = from t in Assembly.GetExecutingAssembly().GetTypes()
+                       where t.BaseType.Name == nameof(BaseModel)
+                       select t.Name;
+            foreach(var item in type)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
