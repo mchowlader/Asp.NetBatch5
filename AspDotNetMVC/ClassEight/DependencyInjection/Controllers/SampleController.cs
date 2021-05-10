d7fd66d4ce729056e8e080d63ed68bdddbe60632
MithunCh <mithun.howlader222@gmail.com>
2021-05-10 19:37:43 +0600

    Dependency Injection and mvc config

---
+++ AspDotNetMVC/ClassEight/DependencyInjection/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using DependencyInjection.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    public class SampleController : Controller
+    {
+        private IDatabaseService _databaseService;
+
+        public SampleController(IDatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SampleView()
+        {
+            var name = _databaseService.GetName();
+
+            return View();
+           
+        }
+    }
+}
---
+++ AspDotNetMVC/ClassEight/DependencyInjection/Service/SimpleDatabaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Service
+{
+    public class SimpleDatabaseService : IDatabaseService
+    {
+        public string GetName()
+        {
+            return "SimpleDatabaseService";
+        }
+    }
+}
