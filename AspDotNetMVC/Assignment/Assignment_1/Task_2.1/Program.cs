360f185068c5a8d160136a6703abe4fbc92e2110
MithunCh <mithun.howlader222@gmail.com>
2021-05-17 20:56:15 +0600

    commit

---
+++ AspDotNetMVC/Assignment/Assignment_1/Task_2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReflectionUtility reflectionUtility = new ReflectionUtility();
+            ClassWithPrivateMethod classWithPrivateMethod = new ClassWithPrivateMethod();
+
+            reflectionUtility.CallPrivate(classWithPrivateMethod, "Print", new object[] { "hi" });
+
+        }
+    }
+
+    public class ReflectionUtility
+    {
+        ClassWithPrivateMethod classWithPrivateMethod = new ClassWithPrivateMethod();
+
+        public void CallPrivate(object targetObject, string methodName, object[] args)
+        {
+            var item = classWithPrivateMethod.GetType()
+                .GetMethod("Print", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .Invoke(classWithPrivateMethod, args);
+            
+        }
+    }
+
+    public class ClassWithPrivateMethod
+    {
+        private void Print(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+}
