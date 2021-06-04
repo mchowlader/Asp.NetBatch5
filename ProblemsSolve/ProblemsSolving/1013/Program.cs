aa224e69e272bb9281c5820c70f6f9f47d3cf2e5
MithunCh <mithun.howlader222@gmail.com>
2021-06-05 02:20:31 +0600

    problem solve

---
+++ ProblemsSolve/ProblemsSolving/1013/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var value = input.Split(' ');
+
+            var A = Convert.ToInt32(value[0]);
+            var B = Convert.ToInt32(value[1]);
+            var C = Convert.ToInt32(value[2]);
+
+
+            var temp = A;
+
+            if(A<B || A<C)
+            {
+                if(B<C)
+                {
+                    temp = C;
+                }
+                else
+                {
+                    temp = B;
+                }
+            }
+
+            Console.WriteLine($"{temp} eh o maior");
+
+
+        }
+    }
+}
