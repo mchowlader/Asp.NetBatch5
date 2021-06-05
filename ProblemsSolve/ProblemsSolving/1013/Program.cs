ff85a86d41085dfe169516347d1491a5bb8be248
MithunCh <mithun.howlader222@gmail.com>
2021-06-05 23:41:16 +0600

    save

--- ProblemsSolve/ProblemsSolving/1013/Program.cs
+++ ProblemsSolve/ProblemsSolving/1013/Program.cs
@@ -13,22 +13,24 @@ namespace _1013
             var B = Convert.ToInt32(value[1]);
             var C = Convert.ToInt32(value[2]);
 
+            var AB = (A + B + Math.Abs(A - B))/2;
+            Console.WriteLine(AB);
 
-            var temp = A;
+            //var temp = A;
 
-            if(A<B || A<C)
-            {
-                if(B<C)
-                {
-                    temp = C;
-                }
-                else
-                {
-                    temp = B;
-                }
-            }
+            //if(A<B || A<C)
+            //{
+            //    if(B<C)
+            //    {
+            //        temp = C;
+            //    }
+            //    else
+            //    {
+            //        temp = B;
+            //    }
+            //}
 
-            Console.WriteLine($"{temp} eh o maior");
+            //Console.WriteLine($"{temp} eh o maior");
 
 
         }
