4cedb5c4c522d1d9ffe34b2be8ef8ec6c86c6418
MithunCh <mithun.howlader222@gmail.com>
2021-06-06 13:20:39 +0600

    Ado practices

---
+++ AspDotNetMVC/classFourteen/AdoDotNet/DataBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDotNet
+{
+    public class DataBase
+    {
+
+
+        public void CreateTable(string cmdText, SqlConnection sqlConnection)
+        {
+            using SqlCommand sqlCommand = new SqlCommand(cmdText, sqlConnection);
+
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("Table create successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error here: " + ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Program Done!");
+            }
+        }
+
+        public void DataInsert(string cmdText, SqlConnection sqlConnection)
+        {
+            using SqlCommand sqlCommand = new SqlCommand(cmdText, sqlConnection);
+
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("Data insert Successfully.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Data insert error: " + ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Program Done!");
+            }
+        }
+    }
+}
---
+++ AspDotNetMVC/classFourteen/AdoDotNet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoDotNet
+{
+
+    
+    class Program
+    {
+        private const string connection = "Server=DESKTOP-VCDQ38S\\SQLEXPRESS;Database=AdoDotNet;Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            
+
+            using SqlConnection sqlConnection = new SqlConnection(connection);
+
+            if(sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+
+            var tableCreate = @"  CREATE TABLE dbo.Course
+                           ( Id int IDENTITY(1,100) NOT NULL,                           
+                            Title nvarchar(50) NOT NULL,
+                            CONSTRAINT pk2 PRIMARY KEY(Id)
+                           );";
+
+            var dataInsert = "insert into Course (Title) values ('CSharp')";
+
+            var dataBase = new DataBase();
+            //dataBase.CreateTable(tableCreate, sqlConnection);
+            dataBase.DataInsert(dataInsert, sqlConnection);     
+
+        }
+
+    }
+}
