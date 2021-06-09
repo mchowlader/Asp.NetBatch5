494e8d58b6849f2a478a164faa104087ce85091f
MithunCh <mithun.howlader222@gmail.com>
2021-06-09 17:31:27 +0600

    save

--- ProblemsSolve/ProblemsSolving/1000/Program.cs
+++ ProblemsSolve/ProblemsSolving/1000/Program.cs
@@ -6,7 +6,30 @@ namespace _1000
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            var second = 0;
+            var hour = 0;
+            var minutes = 0;
+
+            minutes = input / 60;
+            input = input % 60;
+
+            hour = input / 60;
+            input = input % 60;
+
+           
+
+            second = input % 60;
+            input = input % 60;
+
+            Console.WriteLine($"{hour}:{minutes}:{second}");
+
+
+
         }
     }
 }
