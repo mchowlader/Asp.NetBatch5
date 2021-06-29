1be1db4683fd14fa528b5621bd21d2202e1e0afd
MithunCh <mithun.howlader222@gmail.com>
2021-06-30 01:14:12 +0600

    save

---
+++ AspDotNetMVC/FinalProject/MVC.Training/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVC.Traning
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
