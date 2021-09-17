7445a86a99334ba3a430cf275553f128696228b5
MithunCh <mithun.howlader222@gmail.com>
2021-09-17 23:45:06 +0600

    save

---
+++ FinalProject/DataImporter/DataImporter.Common/Utilities/CaptchaSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter.Common.Utilities
+{
+    public class CaptchaSettings
+    {
+        public string ClientKey { get; set; }
+        public string ServerKey { get; set; }
+    }
+}
