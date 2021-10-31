0597c8d9a6e6335e5ac258b80d3e2513a00b37ca
MithunCh <mithun.howlader222@gmail.com>
2021-10-31 20:22:03 +0600

    delegates feature revision

---
+++ VivaPreparation/cSharpFeature/Delegates/Printer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Printer
+    {
+        public void Print1(string text)
+        {
+            Console.WriteLine($"==>{text}<==");
+        }
+
+        public void Print2(string text)
+        {
+            Console.WriteLine($"-::{ text}::-");
+        }
+    }
+}
---
+++ VivaPreparation/cSharpFeature/Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+       delegate void Perform(string text);
+
+        static void Main(string[] args)
+        {
+            var printer = new Printer();
+            var text = "Hello World";
+            var logic = new Perform(printer.Print2);
+            ProcessText(text,logic);
+        }
+
+
+        public static void PrintMethod(string text)
+        {
+            Console.WriteLine($">>>  { text}  <<<");
+        }
+        static void ProcessText(string text, Perform perform)
+        {
+            if(!string.IsNullOrWhiteSpace(text))
+            { 
+                perform(text);
+            }
+        }
+    }
+}
