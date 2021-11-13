c3e6b9f7a8d2d160e4f1e6478fba306db36959f2
MithunCh <mithun.howlader222@gmail.com>
2021-11-13 23:47:49 +0600

    save

---
+++ FinalProject/DataImporter/DataImporter.User/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace DataImporter.User.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ FinalProject/DataImporter/DataImporter.User/UserModule.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using DataImporter.User.Contexts;
+using System;
+
+namespace DataImporter.User
+{
+    public class UserModule : Module
+    {
+
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public UserModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>().AsSelf()
+                .WithParameter("connectionString", _connectionString)
+                .WithParameter("migrationAssemblyName", _migrationAssemblyName)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ApplicationDbContext>().As<IApplicationDbContext>()
+                .WithParameter("connectionString", _connectionString)
+                .WithParameter("migrationAssemblyName", _migrationAssemblyName)
+                .InstancePerLifetimeScope();
+
+
+
+            base.Load(builder);
+        }
+
+    }
+}
